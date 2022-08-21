cd64f976edaf78bf09ab866b7c0ab53214444859
HappyLap <90914236+happy-lappy@users.noreply.github.com>
2022-08-21 23:24:52 +0200

    pl

--- cleanit.sh
+++ cleanit.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sleep 5
+
 if [[ $OSTYPE != "linux-gnu" ]]; then
   printf "This Cleanup Script Should Be Run On Ubuntu Runner.\n"
   exit 1
@@ -90,11 +90,11 @@ curl -sL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh 
 sudo rm -rf -- ./uninstall-brew.sh /home/linuxbrew &>/dev/null
 echo "::endgroup::"
 
-echo "::group::Removing NodeJS, NPM & NPX"
+echo "::group::Purging PIPX & PIP packages"
 {
-  sudo npm list -g --depth=0. 2>/dev/null | awk -F ' ' '{print $2}' | awk -F '@[0-9]' '{print $1}' | grep -v "^n$" | sudo xargs npm remove -g
-  yes | sudo n uninstall
-  parallel --use-cpus-instead-of-cores sudo rm -rf {} 2>/dev/null ::: /usr/local/lib/node_modules ::: /usr/local/n ::: /usr/local/bin/n /usr/local/bin/vercel /usr/local/bin/now
+  pipx uninstall-all && sudo pip3 uninstall -q -y pipx
+  find /usr/share /usr/lib ~/.local/lib -depth -type d -name __pycache__ \
+    -exec rm -rf '{}' + 2>/dev/null;
 } &>/dev/null
 echo "::endgroup::"
 
@@ -116,10 +116,16 @@ echo "::endgroup::"
 echo "::group::Clearing Unwanted Environment Variables"
 printf "This However is Not Retained after the Step is finished. So this part might be removed in the future.\n"
 {
-  sudo sed -i -e '/^PATH=/d;/hostedtoolcache/d;/^AZURE/d;/^SWIFT/d;/^DOTNET/d;/DRIVER/d;/^CHROME/d;/HASKELL/d;/^JAVA/d;/^SELENIUM/d;/^GRAALVM/d;/^ANT/d;/^GRADLE/d;/^LEIN/d;/^CONDA/d;/^VCPKG/d;/^ANDROID/d;/^HOMEBREW/d;' /etc/environment
+  sudo sed -i -e '/^PATH=/d;/hostedtoolcache/d;/^AZURE/d;/^SWIFT/d;/^DOTNET/d;/DRIVER/d;/^CHROME/d;/HASKELL/d;/^JAVA/d;/^SELENIUM/d;/^GRAALVM/d;/^ANT/d;/^GRADLE/d;/^LEIN/d;/^CONDA/d;/^VCPKG/d;/^ANDROID/d;/^PIPX/d;/^HOMEBREW/d;' /etc/environment
   sudo sed -i '1i PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' /etc/environment
   sed -i '/HOME\/\.local\/bin/d' /home/runner/.bashrc
   source /home/runner/.bashrc
+} &>/dev/null
+echo "::endgroup::"
+
+echo "::group::Removing NodeJS, NPM & NPX"
+{
+  parallel --use-cpus-instead-of-cores sudo rm -rf {} 2>/dev/null ::: /usr/local/bin/vercel /usr/local/bin/now
 } &>/dev/null
 echo "::endgroup::"
 
